a5e8cba97f55faece0fe39d57d33ae757f7ed795
Damian Grela <deg171169@gmail.com>
2025-12-17 19:54:47 -0300

    empezando a modificar el archivo App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class ="app">
+  <h1>Conversor de Distancia</h1>
+  <input v-model="kilometros" type="number" placeholder="Ingresa kilometros" />
+  <p :style="{color: textColor }">Metros: {{ meters }}</p>
+   <p :style="{color: textColor }">Millas: {{ miles }}</p>
+   </div>
 </template>
 
 <style scoped></style>
